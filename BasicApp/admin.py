6dad6ef1106ea431b6a5cc49a56d14423c924662
baadillahnabil <nabilzero27@gmail.com>
2019-01-18 12:47:52 +0700

    using models, views, urls

--- BasicApp/admin.py
+++ BasicApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from BasicApp.models import Topic, ActiveRecord, Webpage
 
-# Register your models here.
+
+admin.site.register(ActiveRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
--- BasicApp/models.py
+++ BasicApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    url = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ActiveRecord(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
